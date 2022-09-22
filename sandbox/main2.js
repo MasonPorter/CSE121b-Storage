d214ba7fa810b5d11bcc7f27428c68902561784c
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2022-09-22 13:18:57 -0600

    Push #3
    >W02 further learning

--- sandbox/main2.js
+++ sandbox/main2.js
@@ -41,3 +41,18 @@ function addIt(n1, n2) {
 }
 
 console.log(addIt(5, 4));
+
+document.querySelector('img').setAttribute('width', 300);
+
+//Arrays and DOM
+let classes = ["Javascript", "Atoms to Humans", "Graphic Design"];
+
+console.log(classes);
+console.log(classes[1]); //show specific item in console
+
+classes.push("CIT222"); //add course to the classes array
+classes.shift(); //takes off first item
+classes.splice(2,1); // taking out array items in the middle 2 is start index and 1 is how many to take out
+classes.pop(); //take off the end
+
+document.querySelector('#courses').textContent = classes;
