b2c661b83e072bbc95cbe8d624e581812a38b87d
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2022-09-30 00:24:11 -0600

    Push #7
    >Completed Prove for W03

--- tasks/W03/scripts/task3.js
+++ tasks/W03/scripts/task3.js
@@ -2,48 +2,119 @@
 
 /* FUNCTIONS */
 
-// Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
 
-// Step 2: In the function, return the sum of the parameters number1 and number2
+//-------add
+function add(number1, number2) {
+  return parseInt(number1) + parseInt(number2);
+}
 
-// Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
+function addNumbers() {
+  let addend1 = document.getElementById("addend1").value;
+  let addend2 = document.getElementById("addend2").value;
+  let addfunction = add(addend1, addend2);
+  document.getElementById("sum").value = addfunction;
+}
 
-// Step 4: Assign the return value to an HTML form element with an ID of sum
+document.getElementById("addNumbers").addEventListener("click", addNumbers);
 
-// Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
 
-// Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
+//-------subtract
+function subract(number1, number2) {
+  return parseInt(number1) - parseInt(number2);
+}
 
-// Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
+function subtractNumbers() {
+  let minuend = document.getElementById("minuend").value;
+  let subtrahend = document.getElementById("subtrahend").value;
+  let minusfunction = subract(minuend, subtrahend);
+  document.getElementById("difference").value = minusfunction;
+}
 
-// Step 8: Using any of the three function declaration types, repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor, quotient and divideNumbers
+document.getElementById("subtractNumbers").addEventListener("click", subtractNumbers);
 
-// Step 9: Test all of the mathematical functionality of the task3.html page.
+
+//--------multiply
+function multiply(number1, number2) {
+  return parseInt(number1) * parseInt(number2);
+}
+
+function multiplyNumbers() {
+  let factor1 = document.getElementById("factor1").value;
+  let factor2 = document.getElementById("factor2").value;
+  let multiplyfunction = multiply(factor1, factor2);
+  document.getElementById("product").value = multiplyfunction;
+}
+
+document.getElementById("multiplyNumbers").addEventListener("click", multiplyNumbers);
+
+//----------divide
+function divide(number1, number2) {
+    return parseInt(number1) / parseInt(number2);
+  }
+  
+function divideNumbers() {
+  let dividend = document.getElementById("dividend").value;
+  let divisor = document.getElementById("divisor").value;
+  let dividefunction = divide(dividend, divisor);
+  document.getElementById("quotient").value = dividefunction;
+}
+
+document.getElementById("divideNumbers").addEventListener("click", divideNumbers);
 
 
 /* BUILT-IN METHODS */
 
-// Step 1: Declare and instantiate a variable of type Date to hold the current date
-
-// Step 2: Declare a variable to hold the current year
-
-// Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
-
-// Step 4: Assign the current year variable to an HTML form element with an ID of year
-
+let currentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = currentYear;
 
 /* ARRAY METHODS */
 
 // Step 1: Declare and instantiate an array variable to hold the numbers 1 through 25
+let numberArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
 // Step 2: Assign the value of the array variable to the HTML element with an ID of "array"
+document.getElementById('array').textContent = numberArray;
 
 // Step 3: Use the filter array method to find all of the odd numbers of the array variable and assign the reult to the HTML element with an ID of "odds" ( hint: % (modulus operartor) )
+let odds = numberArray.filter(getOdds);
+
+function getOdds(item) {
+  if (item % 2 === 1) {
+    return item;
+  }
+}
+
+document.getElementById("odds").textContent = odds;
 
 // Step 4: Use the filter array method to find all of the even numbers of the array variable and assign the result to the HTML element with an ID of "evens"
+let evens = numberArray.filter(getEvens);
+
+function getEvens(item) {
+  if (item % 2 === 0) {
+    return item;
+  }
+}
+
+document.getElementById("evens").textContent = evens;
 
 // Step 5: Use the reduce array method to sum the array variable elements and assign the result to the HTML element with an ID of "sumOfArray"
+const arrayTotal = numberArray.reduce(function (total, item) {
+    return total + item;
+})
+
+document.getElementById('sumOfArray').textContent = arrayTotal;
 
 // Step 6: Use the map array method to multiple each element in the array variable by 2 and assign the result to the HTML element with an ID of "multiplied"
+const mapArray = numberArray.map(arrayTimesTwo);
 
+function arrayTimesTwo (num){
+    return num * 2;
+}
+
+document.getElementById('multiplied').textContent = mapArray;
 // Step 7: Use the map and reduce array methods to sum the array elements after multiplying each element by two.  Assign the result to the HTML element with an ID of "sumOfMultiplied"
+const sumTimesTwo = mapArray.reduce(function (total, item) {
+    return total + item;
+})
+
+document.getElementById('sumOfMultiplied').textContent = sumTimesTwo;
