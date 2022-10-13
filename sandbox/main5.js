8d1a0170d27ff3a3eb975ce196eb1e96aefffe9e
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2022-10-13 13:42:12 -0600

    Push #12
    >Finished W05 Notes

--- sandbox/main5.js
+++ sandbox/main5.js
@@ -17,6 +17,7 @@ console.log("synchronous");
 
 
 // fetch.js
+//doesn't need to be url. This can be whatever you want.
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 let results = null;
 async function getPokemon(url) {
@@ -40,3 +41,88 @@ function doStuff(results) {
 
 getPokemon(url);
 console.log("second: ", results);
+
+
+//sorting
+
+let fruit = ['bannana', 'apple', 'strawberry'];
+
+fruit.sort();
+
+console.log(fruit);
+
+
+const animals = [
+  {
+    name: "Meowsy",
+    species: "cat",
+    foods: {
+      likes: ["tuna", "catnip"],
+      dislikes: ["ham", "zucchini"],
+    },
+  },
+
+  {
+    name: "Barky",
+    species: "dog",
+    foods: {
+      likes: ["bones", "carrots"],
+      dislikes: ["tuna"],
+    },
+  },
+
+  {
+    name: "Purrpaws",
+    species: "cat",
+    foods: {
+      likes: ["mice"],
+      dislikes: ["cookies"],
+    },
+  },
+];
+
+console.log(animals[1].name + "likes" + animals[1].foods.likes[0]);
+
+function sortBy() {
+  document.querySelector("#pets").innerHTML = "";
+  let sort = document.querySelector("#sortBy").value;
+  if (sort == "petNameAscending") {
+    outputPets(animals.sort(animal_sort_asc));
+  } else if (sort == "petNameDescending") {
+    outputPets(animals.sort(animal_sort_desc));
+  }
+}
+
+function animal_sort_asc(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  } else if (a.name > b.name) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function animal_sort_desc(a, b) {
+  if (a.name < b.name) {
+    return 1;
+  } else if (a.name > b.name) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+function outputPets() {
+  animals.forEach(function (item) {
+    let petSelection = document.querySelector("#pets");
+    let petName = document.createElement("h2");
+    let petSpecies = document.createElement("h3");
+    petName.textContent = item.name;
+    petSpecies.textContent = item.species;
+    petSelection.appendChild(petName);
+    petSelection.appendChild(petSpecies);
+  });
+}
+
+document.querySelector("#sortBy").addEventListener("change", sortBy);
