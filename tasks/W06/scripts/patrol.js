e5777bdfae50f069530a2a8be08c6c90578b4ddb
JMC Nation <77704338+MasonPorter@users.noreply.github.com>
2022-10-22 19:59:41 -0600

    Push #18
    >Shorted json
    >Made headway on JavaScript

--- tasks/W06/scripts/patrol.js
+++ tasks/W06/scripts/patrol.js
@@ -1,11 +1,28 @@
-const file = "json/resident_data.json";
-let results = null;
-async function getResident(file) {
-    const response =  await fetch(file);
+const resident_data = "https://raw.githubusercontent.com/MasonPorter/CSE121b-Storage/main/tasks/W06/json/resident_data.json";
+async function getResident(resident_data) {
+    const response =  await fetch(resident_data);
     if (response.ok) {
         const data = await response.json();
         printResident(data);
     }
 }
+getResident(resident_data);
 
-getResident(file);
+function printResident(results) {
+  results.forEach(function (item) {
+    let output = document.getElementById('entry-output');
+    let personName = document.createElement("h5");
+    personName.textContent = item.name;
+    output.appendChild(personName);
+  });
+}
+
+const buttonElement = document.getElementById("submit");
+
+function copyInput() {
+    const inputElement = document.getElementById('fullname');
+    const outputElement = document.getElementById('name');
+    outputElement.innerHTML = inputElement.value;
+}
+
+buttonElement.addEventListener("click", copyInput);
